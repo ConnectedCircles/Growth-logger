d8198accdae9a118f932969268f7c09eb30a9107
ConnectedCircles <130466104+ConnectedCircles@users.noreply.github.com>
2023-04-17 12:58:19 +0200

    Update Growth_invite_logger.py

--- Growth_invite_logger.py
+++ Growth_invite_logger.py
@@ -7,7 +7,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Set up Google Sheets credentials
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("growth-logger-f8043abda96b.json", scope)
+creds = Credentials.from_service_account_file('creds.json')
 client = gspread.authorize(creds)
 
 # Define function to append data to Google Sheet
